86feea35ca508baaf591d4a53f65873ffb571686
AlbertBarrachina <87174156+AlbertBarrachina@users.noreply.github.com>
2024-03-14 17:52:42 +0100

    14/3/2024 17:52h

--- TalenTry/app/Http/Controllers/Controller.php
+++ TalenTry/app/Http/Controllers/Controller.php
@@ -5,8 +5,21 @@ namespace App\Http\Controllers;
 use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Routing\Controller as BaseController;
+use App\Http\Controllers\ControllerCategory;
 
 class Controller extends BaseController
 {
     use AuthorizesRequests, ValidatesRequests;
+
+// Display the specified resource.
+public function showRandomQuestions($id)
+{
+    try {
+        $controllerCategory = new ControllerCategory();
+        $QA = $controllerCategory->index($id);
+        return response()->json([$QA], 200);
+    } catch (\Throwable $th) {
+        return response()->json(['message' => 'Ha ocurrido un error al cargar la entrevista sin preguntas: ' . $th->getMessage()], 500);
+    }
+}
 }
--- TalenTry/app/Http/Controllers/ControllerCategory.php
+++ TalenTry/app/Http/Controllers/ControllerCategory.php
@@ -4,8 +4,26 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
+use App\Models\ModelCategory;
 
 class ControllerCategory extends Controller
 {
-    //
+
+    public function index(){
+        try {
+            $QA = ModelCategory::findOrFail();
+            return response()->json([$QA], 200);
+        } catch (\Throwable $th) {
+            return response()->json(['message' => 'Ha ocurrido un error al cargar la entrevista sin preguntas: ' . $th->getMessage()], 500);
+        }
+    }
+
+    public function show($id){
+        try {
+            $QA = ModelCategory::findOrFail($id);
+            return response()->json([$QA], 200);
+        } catch (\Throwable $th) {
+            return response()->json(['message' => 'Ha ocurrido un error al cargar la entrevista sin preguntas: ' . $th->getMessage()], 500);
+        }
+    }
 }
