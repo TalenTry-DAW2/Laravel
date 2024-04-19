b2d52d9dcd1a20a0915f5ac3015c39398150c158
pierosanz2 <128500925+pierosanchez098@users.noreply.github.com>
2024-04-19 15:06:05 +0200

    update seeder record 2 usuarios

--- TalenTry/database/seeders/SeederRecord.php
+++ TalenTry/database/seeders/SeederRecord.php
@@ -15,20 +15,33 @@ class SeederRecord extends Seeder
      */
     public function run(): void
     {
-        {
-            for ($i=0; $i < 10; $i++) { 
-                $numeroAleatorio2 = random_int(1,10);
-                $startDate = Carbon::createFromFormat('m/d/Y', '1/1/2024')->format('Y-m-d');
-                $finishDate = Carbon::createFromFormat('m/d/Y', '1/1/2024')->format('Y-m-d');
-                DB::table('Record')->insert([
-                    'UserID' => 1,
-                    'CategoryID'=> 2,
-                    'score' => $numeroAleatorio2,
-                    'StartDate' => $startDate,
-                    'FinishDate' => $finishDate,
-                ]);
-            }
+        $user1_id = DB::table('users')->where('DNI', '12345678A')->value('UserID');
+        $user2_id = DB::table('users')->where('DNI', '12345678B')->value('UserID');
 
+        for ($i = 0; $i < 5; $i++) {
+            $numeroAleatorio = random_int(1, 10);
+            $startDate = Carbon::createFromFormat('m/d/Y', '1/1/2024')->format('Y-m-d');
+            $finishDate = Carbon::createFromFormat('m/d/Y', '1/1/2024')->format('Y-m-d');
+            DB::table('Record')->insert([
+                'UserID' => $user1_id,
+                'CategoryID' => 2,
+                'score' => $numeroAleatorio,
+                'StartDate' => $startDate,
+                'FinishDate' => $finishDate,
+            ]);
+        }
+
+        for ($i = 0; $i < 5; $i++) {
+            $numeroAleatorio = random_int(1, 10);
+            $startDate = Carbon::createFromFormat('m/d/Y', '1/1/2024')->format('Y-m-d');
+            $finishDate = Carbon::createFromFormat('m/d/Y', '1/1/2024')->format('Y-m-d');
+            DB::table('Record')->insert([
+                'UserID' => $user2_id,
+                'CategoryID' => 2,
+                'score' => $numeroAleatorio,
+                'StartDate' => $startDate,
+                'FinishDate' => $finishDate,
+            ]);
         }
     }
 }
